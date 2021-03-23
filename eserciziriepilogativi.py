ca9b77e4db8dd8f4f7cfd00af5b3faa874aeb7dc
VittorioIannuzzi <iannuzzi.vittorio.s16@liceoviconapoli.it>
2021-03-23 18:41:07 +0100

    Create eserciziriepilogativi.py

---
+++ eserciziriepilogativi.py
@@ -0,0 +1,14 @@
+voti  = {
+    "Mario" : { "matematica" : "6" , "italiano" : "6" , "scienze" : "7" , "inglese" : "4" }    
+    "Giovanni": {" matematica ": " 4 ", " italiano ": " 6 ", " scienze ": " 5 ", " inglese ": " 7 " }
+    "Paola" : { "matematica" : "9" , "italiano" : "6" , "scienze" : "8" , "inglese" : "8" }
+}
+
+m  =  mean [ voti . elementi ( "Mario" )]
+g  =  mean [ voti . elementi ( 'Giovanni' )]
+p  =  mean [ voti . articoli ( 'Paola' )]
+
+print ( "La media dei valori di Marco è" , m )
+print ( "La media 
+( "La media dei valori di Giovanni è" , g )
+print ( "La media dei valori di Paola è" , p )
